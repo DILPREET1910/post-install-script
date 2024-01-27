1ddb1d430f3d1c237bca755bdde7cf65de0e825a
DILPREET1910 <dsahney1910@gmail.com>
2024-01-27 14:40:03 +0530

    check for distro and set installer

--- install.sh
+++ install.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+##########################################################
 # check if root
+##########################################################
 if [ "$EUID" -ne 0 ]
 then echo "Please runt as root"
     exit
 fi
 
-#############################
+##########################################################
+# check for distro type
+##########################################################
+if  grep -q "ID=arch" < /etc/os-release 
+    then DISTRO="arch"
+elif grep -q "ID=ubuntu" < /etc/os-release && grep -q "ID_LIKE=debian" < /etc/os-release
+    then DISTRO="debian"
+fi
+
+##########################################################
+# set installer
+##########################################################
+if [ "$DISTRO" = "arch" ]
+     then INSTALLER="pacman -S "
+elif [ "$DISTRO" = "debian" ]
+    then INSTALLER="apt-get install "
+fi
+
+##########################################################
 # ZSH
-#############################
+##########################################################
 # remove if .config/zsh already exists
 rm -rf .config/zsh
 
