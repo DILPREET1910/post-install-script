3e205c57e0f8061e58508b1a532c994da358e7bb
DILPREET1910 <dsahney1910@gmail.com>
2024-01-31 08:50:53 +0530

    cp neovim config

--- install.sh
+++ install.sh
@@ -98,3 +98,6 @@ fi
 # install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# move the cloned nvim config files to .confg
+mv ./.config/nvim ~/.config/
